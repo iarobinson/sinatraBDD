0ca04bd46f49202d389348e06df250fdf7931fea
Ian Robinson <ian@ianrobinson.net>
2023-01-12 12:25:00 -0800

    Add missing work

--- app.rb
+++ app.rb
@@ -2,6 +2,6 @@ require 'sinatra'
 require 'sinatra/contrib'
 
 get '/' do
-  @server_generated_text = "An example of potentially dynamically rendered by the server."
+  @server_generated_text = "An example of potentially dynamically rendered text by the server."
   erb :index
 end
